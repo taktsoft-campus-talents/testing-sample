a877bf3224762e0563b63eb5a939a34312b41231
Sascha Rose <sascha.rose@gmail.com>
2025-03-24 08:36:52 +0100

    configure workspace

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,17 +1,27 @@
 import { defineWorkspace } from "vitest/config";
 
 export default defineWorkspace([
-  // If you want to keep running your existing tests in Node.js, uncomment the next line.
-  // 'vite.config.ts',
+  // ------------------------------
   {
     extends: "vite.config.ts",
     test: {
+      name: "unit",
+      include: ["src/**/*.test.ts"],
+      environment: "node",
+    },
+  },
+  // ------------------------------
+  {
+    extends: "vite.config.ts",
+    test: {
+      name: "browser",
+      include: ["src/**/*.test.tsx"],
       browser: {
         enabled: true,
-        provider: "playwright",
-        // https://vitest.dev/guide/browser/playwright
+        provider: "playwright", // https://vitest.dev/guide/browser/playwright
         instances: [{ browser: "chromium" }],
       },
     },
   },
+  // ------------------------------
 ]);
